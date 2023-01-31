0aaf2d513940a8e97849732f604ce03ac3a7b870
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-31 15:49:27 +0100

    Add Home Page and About Page

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ app.use(express.static(__dirname + "/public"))
 
 app.get("/",(req,res) => {
     console.log("This is the homepage")
-    res.send("<h1>This is the Jack Johnson homepage</h1>")
-    //res.sendFile(__dirname + "/views/index.html")
+    //res.send("<h1>This is Jack Johnson</h1>")
+    res.sendFile(__dirname + "/views/home.html")
 }) 
 
-/* app.get("/dog", (req,res) => {
-    res.sendFile(__dirname + "/views/dog.html")
-}) */
+app.get("/about", (req,res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (req,res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+
 
 app.listen(port, () => {
 console.log(`listening on port ${port}`)
